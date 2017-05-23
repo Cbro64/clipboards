ff2a22aed5e7043f62834f06476dc668da3c6cdd
Cameron Brown <chickentar@hotmail.com>
2017-05-23 19:14:39 +1000

    works as copy paste now, using ctrl+shift+num to save and alt+shift+num to paste. Only stores last save

--- content.js
+++ content.js
@@ -8,9 +8,27 @@ function getSelectionText() {
     return text;
 }
 
-function checkKey(e) {
-    e = e || window.event;
-    alert(getSelectionText());
+function pasteAtCursor(toPaste) {
+    var el = document.activeElement; // get current text field
+    var start = el.selectionStart;
+    var end = el.selectionEnd;
+    var text = el.value;
+    var before = text.substring(0, start); // text already there, before the cursor
+    var after  = text.substring(end, text.length); // text already there, after the cursor
+    el.value = (before + toPaste + after); // replace old contents with inserted text
+    el.selectionStart = el.selectionEnd = start + newText.length; // move cursor to end of inserted text
+    el.focus();
 }
 
-document.onkeypress = checkKey;
+var saved;
+function checkKey(e) {
+    e = e || window.event;
+    var selection = getSelectionText();
+    if (e.shiftKey && e.ctrlKey && e.keyCode >= 48 && e.keyCode <= 57) { // only react to number keys
+        saved = selection;
+    } else if (e.shiftKey && e.altKey && e.keyCode >= 49 && e.keyCode <= 58) {
+	pasteAtCursor(saved);
+    }
+}
+
+document.onkeydown = checkKey;
